abc200ff65ad8df274ba19c792abf13fb8a06bd2
hydroEng <tahasoomro888@gmail.com>
2023-03-19 10:28:16 +1100

    update tests

--- test/test.py
+++ test/test.py
@@ -2,15 +2,17 @@ import unittest
 import os
 import pandas as pd
 from tuflow_ensemble import tuflow_ensemble
-import sys
 import pathlib
+
 wd = pathlib.Path(__file__).parent.resolve()
+
+sample_data = os.path.join(wd, 'sample_data')
 
 
 class testParsePoCSV(unittest.TestCase):
     def test_parse_po_csv(self):
         expected_output = pd.read_pickle(os.path.join(wd, 'df_parse_po_csv.pickle'))
-        input_csv = os.path.join("sample_data", "Example_010.0Y_10m_tp01_001_PO.csv")
+        input_csv = os.path.join(sample_data, "Example_010.0Y_10m_tp01_001_PO.csv")
         actual_output = tuflow_ensemble.parse_po_csv(input_csv)
         pd.testing.assert_frame_equal(expected_output, actual_output)
 
